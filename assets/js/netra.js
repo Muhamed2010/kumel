64dcbcb121b20c2eb12dbb6f69bc042f09aaa4cf
Arie <arie@wego.com>
2013-02-04 23:36:46 +0700

    update

--- assets/js/netra.js
+++ assets/js/netra.js
@@ -15,20 +15,7 @@ $container.imagesLoaded( function(){
 
 
 });
-$(function() {
-    $( "#city" ).autocomplete({
-      source: function( request, response ) {
-        $.ajax({ 
-          url: "<?php echo base_url(); ?>index.php/main/kota",
-          data: { "q": request.term },
-          success: function(data) { 
-            response(data); 
-          }
-        });
-      },
-      minLength: 2,
-    });
-  });
+
 
 ///////////////--------------------------------------------->drop down
 
