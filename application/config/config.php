d470008f6d3ab30c2db642bb3bd9dcbec46a6b5a
Arie <arie@wego.com>
2013-02-08 15:01:17 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://tripify.me/kumel/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
