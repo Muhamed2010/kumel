b432a147cdc26eabc2fceed63d199f7640eb179c
Arie <arie@wego.com>
2013-02-10 10:48:21 +0700

    update

--- application/views/layout/deals.php
+++ application/views/layout/deals.php
@@ -2,7 +2,7 @@
 <div class="container deals">
   <?php
 
-  $url = 'https://api.roomorama.com/v1.0/rooms.json?destination_id=1&limit=100';
+  $url = 'https://api.roomorama.com/v1.0/rooms.json?destination_id=1&limit=1';
   $content = file_get_contents($url);
   $json = json_decode($content, true);
 
@@ -10,14 +10,14 @@
     print '<a href="'.$item['url'].'" title="'.$item['title'].'"target=_blank>';
     print'<div class="boox">';
 
-      print'<div class="tag-currency">'.$item['price'];
-      print'</div>';
- 
+    print'<div class="tag-currency">'.$item['price'];
+    print'</div>';
 
-      print '<img class="lazy" src="' . $item['thumbnail'] . '" border="0" alt="" />';
+
+    print '<img class="lazy" src="' . $item['thumbnail'] . '" border="0" alt="" />';
     print '<div class="banner">';
     print'<div class="tag-title">"'.$item['title'].'"</div>';
-     print'<div class="tag-location">"'.$item['city'].'"</div>';
+    print'<div class="tag-location">"'.$item['city'].'"</div>';
     print'</div>';
 
     print '</div>';
@@ -26,5 +26,5 @@
   } ?>
 
   
- 
+
 </div>
