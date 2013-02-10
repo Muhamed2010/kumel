e3e1bf44c3fa25f9cf5985a51eabb15d97e3c6ae
Arie <arie@wego.com>
2013-02-10 10:50:51 +0700

    no message

--- application/controllers/deals.php
+++ application/controllers/deals.php
@@ -4,7 +4,7 @@ class Deals extends MY_Controller
 
     public function index()
     {
-    	//error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+    	error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
         $this->render('common/header');
         $this->render('layout/deals');
         $this->render('common/footer');
