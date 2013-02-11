93953308b92f029a3f1fe08a482ea8a19a1b98ac
Arie <arie@wego.com>
2013-02-11 22:38:55 +0700

    no message

--- application/views/search.php
+++ application/views/search.php
@@ -6,7 +6,7 @@
 		<div class="right-block">
 
 			<div class="image-cover">
-					<div class="tag-currency">Rp <?php echo $objek->price?>/ Orang</div>	
+				<div class="tag-currency">Rp <?php echo $objek->price?>/ Orang</div>	
 				<img class="lazy" src="" data-original="http://www.photoshelter.com/img-get/I0000qlJ1FH_YGsw/s/590/392/North-Maluku-104.jpg">
 
 			</div>
