be975eb4bf0a393166355b5f1b86d4c2947ee355
Arie <arie@wego.com>
2013-02-11 21:23:59 +0700

    no message

--- application/views/search.php
+++ application/views/search.php
@@ -1,20 +1,23 @@
 <div class="container result">
 	
-		<?php foreach($objeks as $objek): ?>
-		<div class="boox">
-			<div class="right-block">
+	<?php foreach($objeks as $objek): ?>
+	<div class="boox">
 
-				<div class="image-cover">
+		<div class="right-block">
+
+			<div class="image-cover">
+					<div class="tag-currency">Rp <?php echo $objek->price?>/ Orang</div>	
 				<img class="lazy" src="" data-original="http://www.photoshelter.com/img-get/I0000qlJ1FH_YGsw/s/590/392/North-Maluku-104.jpg">
-				</div>
-				<div class="banner">
+
+			</div>
+			<div class="banner">
 				<div class="tag-title"><?php echo $objek->post_title?></div>
-				</div>
-				<div class="bottom-banner">
+			</div>
+			<div class="bottom-banner">
 				<div class="tag-location"> City Location, Country Location</div>
-				</div>
 			</div>
 		</div>
-	<?php endforeach; ?>
+	</div>
+<?php endforeach; ?>
 
 </div>
