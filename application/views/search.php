1dbbd157465aa1d6a5bf91a4800d86e4c462361e
Arie <arie@wego.com>
2013-02-11 21:02:02 +0700

    update data

--- application/views/search.php
+++ application/views/search.php
@@ -3,14 +3,16 @@
 		<?php foreach($objeks as $objek): ?>
 		<div class="boox">
 			<div class="right-block">
+
 				<div class="image-cover">
 				<img class="lazy" src="" data-original="http://www.photoshelter.com/img-get/I0000qlJ1FH_YGsw/s/590/392/North-Maluku-104.jpg">
 				</div>
 				<div class="banner">
 				<div class="tag-title"><?php echo $objek->post_title?></div>
-				
 				</div>
+				<div class="bottom-banner">
 				<div class="tag-location"> City Location, Country Location</div>
+				</div>
 			</div>
 		</div>
 	<?php endforeach; ?>
