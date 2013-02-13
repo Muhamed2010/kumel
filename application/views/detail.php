0ca9ff450b78753be3029e0b58fc7c9ff2add05f
Arie <arie@wego.com>
2013-02-13 22:35:23 +0700

    update detail trip

--- application/views/detail.php
+++ application/views/detail.php
@@ -1,7 +1,15 @@
 
 <div class="container detail">
+	<div class="detail-aside">asdadad</div>
 	<div class="detail-content">
+
 		<div class="top-artisan"><h1><?php echo  $objek->post_title;?></h1></div>
+		<div class="trip-info">
+			<div class="trip-details"><i class="icon-map-marker"></i> Malang, Indonesia</div>
+			<div class="trip-details"><i class="icon-time"></i>1 Hari</div>
+			<div class="trip-details"><i class="icon-tags"></i> Kegiatan Outdoor</div>
+			<div class="trip-details"><i class="icon-heart"></i> 120 Komentar</div>
+		</div>
 		<div class="image-slider"></div>
 	</div>
 	<div class="detail-content">
@@ -9,4 +17,6 @@
 		<p><?php echo  $objek->content_2; ?><p>
 		<p><?php echo  $objek->content_3; ?><p>
 	</div>
+
+
 </div>
