731c25c70364b266cbbbc49e2d03cacd22f5e4ef
Arie <arie@wego.com>
2013-02-13 22:03:34 +0700

    no message

--- application/views/detail.php
+++ application/views/detail.php
@@ -3,9 +3,10 @@
 	<div class="detail-content">
 		<div class="top-artisan"><h1><?php echo  $objek->post_title;?></h1></div>
 		<div class="image-slider"></div>
+	</div>
+	<div class="detail-content">
 		<p><?php echo  $objek->content_1; ?><p>
 		<p><?php echo  $objek->content_2; ?><p>
 		<p><?php echo  $objek->content_3; ?><p>
-	<div>
-		
+	</div>
 </div>
