2edffedea661752923f603509e297b1071c4ba30
Arie <arie@wego.com>
2013-02-13 17:26:17 +0700

    no message

--- application/views/detail.php
+++ application/views/detail.php
@@ -1,10 +1,9 @@
-<div class="container-big">
-<img class="lazy" src="" data-original="http://www.photoshelter.com/img-get/I0000qlJ1FH_YGsw/s/590/392/North-Maluku-104.jpg" id="big-dady">
-</div>
+
 <div class="container detail">
 	<h2> <?php echo  $objek->post_title;?></h1>
 	<div class="detail-content">
 		
+		<div class="image-slider"></div>
 		<p><?php echo  $objek->content_1; ?><p>
 		<p><?php echo  $objek->content_2; ?><p>
 		<p><?php echo  $objek->content_3; ?><p>
