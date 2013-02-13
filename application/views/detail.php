6c0e8a561e5d329c99f7f9499ffd8265cffa30a5
Arie <arie@wego.com>
2013-02-13 21:52:10 +0700

    /Applications/XAMPP/xamppfiles/htdocs/product/kumel/application/views/detail.php /Applications/XAMPP/xamppfiles/htdocs/product/kumel/assets/css/common.css /Applications/XAMPP/xamppfiles/htdocs/product/kumel/assets/css/common.less

--- application/views/detail.php
+++ application/views/detail.php
@@ -1,11 +1,11 @@
 
 <div class="container detail">
-	<h2> <?php echo  $objek->post_title;?></h1>
 	<div class="detail-content">
-		
+		<div class="top-artisan"><h1><?php echo  $objek->post_title;?></h1></div>
 		<div class="image-slider"></div>
 		<p><?php echo  $objek->content_1; ?><p>
 		<p><?php echo  $objek->content_2; ?><p>
 		<p><?php echo  $objek->content_3; ?><p>
 	<div>
+		
 </div>
