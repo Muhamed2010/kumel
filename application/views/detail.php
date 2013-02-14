87654b7675f1fe15606e8650ba56bc1cf0770578
Arie <arie@wego.com>
2013-02-14 21:18:30 +0700

    update aside

--- application/views/detail.php
+++ application/views/detail.php
@@ -4,27 +4,27 @@
 		<div class="aside-content">
 			<div class="price-area">
 				
-					<h2 class="price">Rp <?php echo number_format($objek->price, 2, ',', '.'); ?></h2>
-					<div class="top-label">Jumlah Traveller</div>
-					<div class="form-input"><input type="text" id="tanggal"  class="input-small"></div>
-					<div class="top-label">Tanggal Berangkat</div>
-					<div class="form-input"><input type="text" class="input-large" id="order-date"></div>
+				<h2 class="price">Rp <?php echo number_format($objek->price, 2, ',', '.'); ?></h2>
+				<div class="top-label">Jumlah Traveller</div>
+				<div class="form-input"><input type="text" id="tanggal"  class="input-small"></div>
+				<div class="top-label">Tanggal Berangkat</div>
+				<div class="form-input"><input type="text" class="input-large" id="order-date"></div>
 			</div>
 			<button class="btn btn-large btn-block btn-primary" type="button">Pemesanan Cepat</button>
 		</div>
-			<div class="aside-content">
+		<div class="aside-content">
 			<div class="price-area">
 				<div class="profile-picture">
-				<img  src="http://media-cache-ec1.pinterest.com/avatars/vanbungkring_1331655668_600.jpg">
+					<img  src="http://media-cache-ec1.pinterest.com/avatars/vanbungkring_1331655668_600.jpg">
 				</div>
 				<div class="provider-name"> Vanbungkring</div>
 				<div class="provider-location">Jakarta, Indonesia</div>
 			</div>
 			<button class="btn btn-large btn-block btn-primary" type="button">Pemesanan Cepat</button>
 		</div>
-			<div class="aside-content">
-			
-			</div>
+		<div class="aside-other-trip">
+			<div class="header-vans"> Vanbungkring's Other Trip</div>
+		</div>
 	</div>
 	<div class="detail-content">
 
@@ -43,14 +43,14 @@
 			<li class="active"><a href="#home">Rincian Perjalanan</a></li>
 			<li><a href="#profile">Itenerary</a></li>
 			<li><a href="#messages">Rincian harga</a></li>
-		
+
 		</ul>
 		<div class="tab-content">
-  <div class="tab-pane active" id="home"><p><?php echo  $objek->content_1; ?></p></div>
-  <div class="tab-pane" id="profile"><p><?php echo  $objek->content_2; ?></p></div>
-  <div class="tab-pane" id="messages"><p><?php echo  $objek->content_3; ?></p></div>
-  
-</div>
+			<div class="tab-pane active" id="home"><p><?php echo  $objek->content_1; ?></p></div>
+			<div class="tab-pane" id="profile"><p><?php echo  $objek->content_2; ?></p></div>
+			<div class="tab-pane" id="messages"><p><?php echo  $objek->content_3; ?></p></div>
+
+		</div>
 	</div>
 
 
